77891332b2f130550db7d44d8ec65a87b1cd9ea9
Ahmed-Alshiba <amashiba@gmail.com>
2023-08-24 18:00:51 +0530

    add background, pattern, make nav collapseable

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -28,3 +28,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
   });
 });
+
+// collapse menu
+var coll = document.getElementsByClassName("collapse-btn");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
